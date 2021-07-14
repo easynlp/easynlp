a9ab3903f509b1850760db7dc9dc26f8641e9862
bentrevett <bentrevett@gmail.com>
2021-07-14 12:47:13 +0100

    fix classification output

--- src/easynlp/classification.py
+++ src/easynlp/classification.py
@@ -49,5 +49,5 @@ def classification(
 
 def get_classification(examples, pipe, input_column, labels, output_column):
     outputs = pipe(examples[input_column], labels)
-    predicted_classes = [outputs["labels"][0] for output in outputs]
+    predicted_classes = [output["labels"][0] for output in outputs]
     return {output_column: predicted_classes}
