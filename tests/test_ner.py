e270b3cf68ee10b09bb330ae802cee10643dd468
bentrevett <bentrevett@gmail.com>
2021-07-15 10:50:40 +0100

    add ner test for single example

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -1,6 +1,24 @@
 import easynlp
 
 
+def test_single_ner():
+    data = {
+        "text": [
+            "My name is Ben. I live in Scotland and work for Microsoft.",
+        ]
+    }
+    input_column = "text"
+    output_column = "ner_tags"
+    output_dataset = easynlp.ner(data, input_column, output_column)
+    ner_tags = [["PER", "LOC", "ORG"]]
+    ner_tags_starts = [[11, 26, 48]]
+    ner_tags_ends = [[14, 34, 57]]
+    assert len(output_dataset) == 1
+    assert output_dataset[output_column] == ner_tags
+    assert output_dataset[f"{output_column}_starts"] == ner_tags_starts
+    assert output_dataset[f"{output_column}_ends"] == ner_tags_ends
+
+
 def test_ner():
     data = {
         "text": [
