2e110e28634ebc2a35dc84e41b648693a9674e5b
bentrevett <bentrevett@gmail.com>
2021-07-19 22:30:46 +0100

    add type hints + comments to summarization

--- src/easynlp/summarization.py
+++ src/easynlp/summarization.py
@@ -13,26 +13,33 @@ def summarization(
 ):
     """Does summarization on data."""
 
+    # get default model name
     if model_name is None:
         model_name = f"google/pegasus-xsum"
 
+    # check input and output columns are different
     assert (
         input_column != output_column
     ), f"input and output columns must be different, both are {input_column}"
 
+    # convert data to datasets.Dataset
     dataset = handle_data(data)
 
+    # remove all columns that aren't the `input_column`
     columns_to_remove = [f for f in dataset.features if f != input_column]
     dataset = dataset.remove_columns(columns_to_remove)
 
+    # load model and tokenizer
     model = transformers.AutoModelForSeq2SeqLM.from_pretrained(model_name)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
+    # create pipeline
     pipe = transformers.SummarizationPipeline(
         model=model,
         tokenizer=tokenizer,
     )
 
+    # perform summarization
     dataset = dataset.map(
         get_summarization,
         fn_kwargs={
@@ -47,7 +54,13 @@ def summarization(
     return dataset
 
 
-def get_summarization(examples, pipe, input_column, output_column):
+def get_summarization(
+    examples: List[Dict[str, List[str]]],
+    pipe: transformers.Pipeline,
+    input_column: str,
+    output_column: str,
+) -> Dict[str, List[str]]:
+    """Performs translation on a batch of examples."""
     outputs = pipe(
         examples[input_column],
         clean_up_tokenization_spaces=True,
